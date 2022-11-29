f5f9137eecdc48ab6c7cc21495e9d140a54be7f9
shobha <shobhaghuge20@gmail.com>
2022-11-29 13:37:59 +0530

    new branch added

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    print("Good morning")
     print_hi('Please subscribe link and share')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
